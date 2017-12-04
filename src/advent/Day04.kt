291827d69c59cec844773c06325af6a02cc981bd
grudus <jakgruda@gmail.com>
2017-12-04 22:31:56 +0100

    Add solution to the 4th day

---
+++ src/advent/Day04.kt
@@ -0,0 +1,52 @@
+package advent
+
+import advent.Day04.firstStar
+import advent.Day04.secondStar
+import java.io.File
+
+/*A new system policy has been put in place that requires all accounts to use a passphrase instead of simply a password. A passphrase consists of a series of words (lowercase letters) separated by spaces.
+
+To ensure security, a valid passphrase must contain no duplicate words.
+
+For example:
+
+aa bb cc dd ee is valid.
+aa bb cc dd aa is not valid - the word aa appears more than once.
+aa bb cc dd aaa is valid - aa and aaa count as different words.
+The system's full passphrase list is available as your puzzle input. How many passphrases are valid?
+
+*/
+/*For added security, yet another system policy has been put in place. Now, a valid passphrase must contain no two words that are anagrams of each other - that is, a passphrase is invalid if any word's letters can be rearranged to form any other word in the passphrase.
+
+For example:
+
+abcde fghij is a valid passphrase.
+abcde xyz ecdab is not valid - the letters from the third word can be rearranged to form the first word.
+a ab abc abd abf abj is a valid passphrase, because all letters need to be used when forming another word.
+iiii oiii ooii oooi oooo is valid.
+oiii ioii iioi iiio is not valid - any of these words can be rearranged to form any other word.
+Under this new system policy, how many passphrases are valid?
+
+*/
+
+
+object Day04 {
+    fun firstStar(input: List<String>) = count(input)
+
+    fun secondStar(input: List<String>) = count(input) { it.toList().sorted() }
+
+    private fun count(input: List<String>, mapper: (String) -> List<Char> = { it.toList() }) =
+            input
+                    .map { it.split(Regex("\\s+")).map(mapper) }
+                    .filter { it.size == it.distinct().size }
+                    .count()
+
+}
+
+fun main(args: Array<String>) {
+    val input = File("src/advent/Day04-input").readLines()
+    println(firstStar(input))
+    println(secondStar(input))
+
+
+}
