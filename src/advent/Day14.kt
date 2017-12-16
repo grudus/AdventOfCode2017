ea235f1beee89c5ee5418afc046883e786de95a2
grudus <jakgruda@gmail.com>
2017-12-16 09:44:33 +0100

    Disk Defragmentation 2nd star

--- src/advent/Day14.kt
+++ src/advent/Day14.kt
@@ -1,5 +1,7 @@
 package advent
 
+import java.lang.Long
+
 /*--- Day 14: Disk Defragmentation ---
 
 Suddenly, a scheduled job activates the system's disk defragmenter. Were the situation different, you might sit and watch it for a while, but today, you just don't have that kind of time. It's soaking up valuable system resources that are needed elsewhere, and so the only option is to help it finish its task as soon as possible.
@@ -29,20 +31,85 @@ In this example, 8108 squares are used across the entire 128x128 grid.
 Given your actual key string, how many squares are used?
 */
 
+/*--- Part Two ---
+
+Now, all the defragmenter needs to know is the number of regions. A region is a group of used squares that are all adjacent, not including diagonals. Every used square is in exactly one region: lone used squares form their own isolated regions, while several adjacent squares all count as a single region.
+
+In the example above, the following nine regions are visible, each marked with a distinct digit:
+
+11.2.3..-->
+.1.2.3.4
+....5.6.
+7.8.55.9
+.88.5...
+88..5..8
+.8...8..
+88.8.88.-->
+|      |
+V      V
+Of particular interest is the region marked 8; while it does not appear contiguous in this small view, all of the squares marked 8 are connected when considering the whole 128x128 grid. In total, in this example, 1242 regions are present.
+
+How many regions are present given your key string?*/
+
 object Day14 {
     private val RANGE = (0..127).toList().toTypedArray()
     private val ASCII_0 = 48
 
     fun firstStar(input: String): Int =
-            (0..127).map { input + "-$it" }
-                    .map { Day10.secondStar(it, RANGE) }
-                    .map { it.map { java.lang.Long.parseLong(it.toString(), 16) } }
-                    .map { it.map { java.lang.Integer.toBinaryString(it.toInt()) } }
+            createMatrix(input)
                     .sumBy { row -> row.sumBy { binary -> binary.sumBy { it.toInt() - ASCII_0 } } }
+
+    fun secondStar(input: String): Int {
+        val matrix: Array<Array<String>> = createMatrix(input)
+                .map { it.flatMap { it.map { if (it == '1') "+" else "-" } }.toTypedArray() }.toTypedArray()
+        var max = 0
+
+        for (row in 0 until 128) {
+            for (col in 0 until 128)
+                if (matrix[row][col] == "+") {
+                    matrix[row][col] = findFirstNotEmpty(matrix, row, col) ?: (++max).toString()
+                    expand(matrix, row, col, matrix[row][col])
+                }
+        }
+        return max
+    }
+
+    private fun expand(matrix: Array<Array<String>>, row: Int, col: Int, value: String) {
+        matrix[row][col] = value
+
+        listOf(Pair(row, col - 1), Pair(row - 1, col), Pair(row, col + 1), Pair(row + 1, col))
+                .filter { it.first >= 0 && it.second >= 0 && it.first < 128 && it.second < 128 }
+                .filter { matrix[it.first][it.second] == "+" }
+                .forEach { expand(matrix, it.first, it.second, value) }
+    }
+
+    private fun createMatrix(input: String): List<List<String>> {
+        return (0..127).map { input + "-$it" }
+                .map { Day10.secondStar(it, RANGE.copyOf()) }
+                .map { it.map { Long.parseLong(it.toString(), 16) } }
+                .map { it.map { toBinaryString(it) } }
+    }
+
+
+    private fun findFirstNotEmpty(matrix: Array<Array<String>>, row: Int, col: Int): String? =
+            adjacentPairs(row, col)
+                    .map { matrix[it.first][it.second] }
+                    .find { it != "-" && it != "+" }
+
+    private fun adjacentPairs(row: Int, col: Int) =
+            listOf(Pair(row, col - 1), Pair(row - 1, col), Pair(row, col + 1), Pair(row + 1, col))
+                    .filter { it.first >= 0 && it.second >= 0 && it.first < 128 && it.second < 128 }
+
+
+    fun toBinaryString(number: kotlin.Long): String {
+        val binary = Integer.toBinaryString(number.toInt())
+        return (0 until (4 - binary.length)).joinToString("") { "0" } + binary
+    }
 }
 
 fun main(args: Array<String>) {
     val input = "nbysizxe"
 
     println(Day14.firstStar(input))
+    println(Day14.secondStar(input))
 }
